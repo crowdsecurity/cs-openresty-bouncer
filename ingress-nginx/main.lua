90c6d942834495e3cae46619834e04a631bf87ef
Laurence Jones <laurence.jones@live.co.uk>
2024-09-18 16:37:01 +0100

    Update main.lua

--- ingress-nginx/main.lua
+++ ingress-nginx/main.lua
@@ -2,7 +2,7 @@ local cs = require "plugins.crowdsec.crowdsec"
 local ngx = ngx
 
 local _M = {}
-local ok, err = cs.init("/etc/nginx/lua/plugins/crowdsec/crowdsec-bouncer.conf", "crowdsec-openresty-bouncer/v1.0.3)
+local ok, err = cs.init("/etc/nginx/lua/plugins/crowdsec/crowdsec-bouncer.conf", "crowdsec-openresty-bouncer/v1.0.3")
 if ok == nil then
     ngx.log(ngx.ERR, "[Crowdsec] " .. err)
     error()
