3091e4613e192e7700aa5a83e196ad21ce3ef5ad
marco <marco@crowdsec.net>
2025-02-25 13:55:32 +0100

    don't require bash

--- docker/docker_start.sh
+++ docker/docker_start.sh
@@ -1,43 +1,42 @@
-#!/bin/bash
+#!/bin/sh
 
-#set -x
 CROWDSEC_BOUNCER_CONFIG="${BOUNCER_CONFIG:-/etc/crowdsec/bouncers/crowdsec-openresty-bouncer.conf}"
 
-params=(
-  API_URL
-  API_KEY
-  CACHE_EXPIRATION
-  BOUNCING_ON_TYPE
-  FALLBACK_REMEDIATION
-  REQUEST_TIMEOUT
-  UPDATE_FREQUENCY
-  MODE
-  EXCLUDE_LOCATION
-  BAN_TEMPLATE_PATH
-  REDIRECT_LOCATION
-  RET_CODE
-  SECRET_KEY
-  SITE_KEY
-  CAPTCHA_TEMPLATE_PATH
-  CAPTCHA_EXPIRATION
-  CAPTCHA_PROVIDER
-  APPSEC_URL
-  APPSEC_FAILURE_ACTION
-  APPSEC_CONNECT_TIMEOUT
-  APPSEC_SEND_TIMEOUT
-  APPSEC_PROCESS_TIMEOUT
-  ALWAYS_SEND_TO_APPSEC
-  SSL_VERIFY
-)
+params='
+ALWAYS_SEND_TO_APPSEC
+API_KEY
+API_URL
+APPSEC_CONNECT_TIMEOUT
+APPSEC_FAILURE_ACTION
+APPSEC_PROCESS_TIMEOUT
+APPSEC_SEND_TIMEOUT
+APPSEC_URL
+BAN_TEMPLATE_PATH
+BOUNCING_ON_TYPE
+CACHE_EXPIRATION
+CAPTCHA_EXPIRATION
+CAPTCHA_PROVIDER
+CAPTCHA_TEMPLATE_PATH
+EXCLUDE_LOCATION
+FALLBACK_REMEDIATION
+MODE
+REDIRECT_LOCATION
+REQUEST_TIMEOUT
+RET_CODE
+SECRET_KEY
+SITE_KEY
+SSL_VERIFY
+UPDATE_FREQUENCY
+'
 
-for var in "${params[@]}"; do
-  value="${!var}"
-  if [[ -n "$value" ]]; then
-    sed -i "s,${var}.*,${var}=${value}," "$CROWDSEC_BOUNCER_CONFIG"
-  fi
+for var in $params; do
+    eval "value=\$$var"
+    if [ -n "$value" ]; then
+        sed -i "s,${var}.*,${var}=${value}," "$CROWDSEC_BOUNCER_CONFIG"
+    fi
 done
 
-
-if [[ "${IS_LUALIB_IMAGE,,}" != "true" ]]; then
+lower=$(echo "$IS_LUALIB_IMAGE" | tr '[:upper:]' '[:lower:]')
+if [ "$lower" != "true" ]; then
     exec /usr/local/openresty/bin/openresty -g "daemon off;"
 fi
