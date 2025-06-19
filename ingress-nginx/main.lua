6589f052454ddc208c3871eca83d5691b8597e0b
sabban <github@sabban.eu>
2025-06-20 00:01:49 +0200

    ensure proper initialization

--- ingress-nginx/main.lua
+++ ingress-nginx/main.lua
@@ -1,7 +1,5 @@
 local cs = require "plugins.crowdsec.crowdsec"
 local ngx = ngx
-local dict = ngx.shared.crowdsec_init or ngx.shared.plugin_locks  -- define in nginx conf
-local init_key = "crowdsec_init_done"
 
 local _M = {}
 local ok, err = cs.init("/etc/nginx/lua/plugins/crowdsec/crowdsec-bouncer.conf", "crowdsec-openresty-bouncer/v1.1.0")
@@ -9,36 +7,28 @@ if ok == nil then
     ngx.log(ngx.ERR, "[Crowdsec] " .. err)
     error()
 end
-
-ensure_initialized()
 ngx.log(ngx.ALERT, "[Crowdsec] Initialisation done")
+
+function _M.init_worker()
+    -- This function is called once per worker
+    -- It can be used to initialize the worker
+    -- For example, to load the configuration file
+    local mode = cs.get_mode()
+    if string.lower(mode) == "stream" then
+        ngx.log(ngx.INFO, "Initializing stream mode for worker " .. tostring(ngx.worker.id()))
+        cs.SetupStream()
+    end
+    cs.debug_metrics()
+
+    ngx.log(ngx.INFO, "Crowdsec bouncer initialized in " .. mode .. " mode for worker " .. tostring(ngx.worker.id()))
+    if ngx.worker.id() == 0 then
+        cs.SetupMetrics()
+    end
+end
+
 
 function _M.rewrite()
 	cs.Allow(ngx.var.remote_addr)
-end
-
-local function ensure_initialized()
-    local ok, err = dict:get(init_key)
-    if ok then
-        return
-    end
-
-    -- try to claim the init
-    local success, err = dict:add(init_key, true)
-    if success then
-        -- this worker does the init
-        cs = require "crowdsec"
-        local mode = cs.get_mode()
-        if string.lower(mode) == "stream" then
-           ngx.log(ngx.INFO, "Initializing stream mode for worker " .. tostring(ngx.worker.id()))
-           cs.SetupStream()
-        end
-
-        if ngx.worker.id() == 0 then
-           ngx.log(ngx.INFO, "Initializing metrics for worker " .. tostring(ngx.worker.id()))
-           cs.SetupMetrics()
-        end
-    end
 end
 
 return _M
