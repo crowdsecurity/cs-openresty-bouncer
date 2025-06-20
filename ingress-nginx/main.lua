e15925deb47b030878e647cf8f0a14122ef0e728
Manuel Sabban <github@sabban.eu>
2025-06-20 15:49:00 +0200

    Bump v1.0.9 (#69)
    * update the lua lib to v1.0.7
    * ensure proper initialization
    * bump v1.0.9
    * revert formatting change

--- ingress-nginx/main.lua
+++ ingress-nginx/main.lua
@@ -2,12 +2,29 @@ local cs = require "plugins.crowdsec.crowdsec"
 local ngx = ngx
 
 local _M = {}
-local ok, err = cs.init("/etc/nginx/lua/plugins/crowdsec/crowdsec-bouncer.conf", "crowdsec-openresty-bouncer/v1.0.4")
+local ok, err = cs.init("/etc/nginx/lua/plugins/crowdsec/crowdsec-bouncer.conf", "crowdsec-openresty-bouncer/v1.1.0")
 if ok == nil then
     ngx.log(ngx.ERR, "[Crowdsec] " .. err)
     error()
 end
 ngx.log(ngx.ALERT, "[Crowdsec] Initialisation done")
+
+function _M.init_worker()
+    -- This function is called once per worker
+    -- It can be used to initialize the worker
+    -- For example, to load the configuration file
+    local mode = cs.get_mode()
+    if string.lower(mode) == "stream" then
+        ngx.log(ngx.INFO, "Initializing stream mode for worker " .. tostring(ngx.worker.id()))
+        cs.SetupStream()
+    end
+
+    if ngx.worker.id() == 0 then
+        cs.SetupMetrics()
+    end
+    ngx.log(ngx.INFO, "Crowdsec bouncer initialized in " .. mode .. " mode for worker " .. tostring(ngx.worker.id()))
+end
+
 
 function _M.rewrite()
 	cs.Allow(ngx.var.remote_addr)
